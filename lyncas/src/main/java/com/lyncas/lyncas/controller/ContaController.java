9bd009b76db59bb38a5aadc15d5e92724749a6bf
marcos.santos <marcos.santos@artit.com.br>
2025-01-04 09:55:27 -0300

    adjust receiver file

--- lyncas/src/main/java/com/lyncas/lyncas/controller/ContaController.java
+++ lyncas/src/main/java/com/lyncas/lyncas/controller/ContaController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +29,16 @@ import com.lyncas.lyncas.service.ContaService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/contas")
@@ -107,7 +117,15 @@ public class ContaController {
     }
     
     @PostMapping("/importar")
-    @Operation(summary = "Importa contas a pagar a partir de um arquivo CSV")
+    @Operation(
+        summary = "Importa contas a pagar a partir de um arquivo CSV",
+        requestBody = @RequestBody(
+            content = @Content(
+                mediaType = "multipart/form-data",
+                schema = @Schema(type = "string", format = "binary")
+            )
+        )
+    )
     @ApiResponse(responseCode = "200", description = "Contas importadas com sucesso")
     public ResponseEntity<String> importarContas(@RequestParam("file") MultipartFile file) {
         contaService.importarContas(file);
